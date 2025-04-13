2fed089aaac3a3bd53b7a1e5b946459449c0fb8d
JeanPhilippeThai <45850151+JeanPhilippeThai@users.noreply.github.com>
2025-04-13 04:12:05 +0200

    Update main.py

--- dev/main.py
+++ dev/main.py
@@ -1,5 +1,14 @@
 def main():
-    print("Hello, World!")
+    df_procurement_monthly = init_procurement_monthly()
+    top_supplier = ytoy_ca_fournisseur(df_procurement_monthly)
+    
+    # Table des 10000 fournisseurs avec la meilleure croissance de CA année après année, avec un minimum de 10000 R$ de CA.
+    # On peut ainsi les repérer et les accompagner pour continuer leur croissance
+    # Ou bien récupérer leurs bonnes pratiques pour les partager aux autres fournisseurs afin de les aider à se lancer
+    turnover_treshhold = 10000
+    top_supplier[top_supplier['turnover_previous_year']>turnover_treshhold].sort_values(by=["percent_ytoy_growth"], ascending=[False]).head(100)
 
+    return 0
+    
 if __name__ == "__main__":
     main()
